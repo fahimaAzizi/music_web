ca241bf7e51859dad63c02ab8e22ba32f999738a
Fahima Azizi <fahimaazizi397@gmail.com>
2024-11-15 23:48:56 +0500

    committed

---
+++ script.js
@@ -0,0 +1,23 @@
+console.log("Let's write JavaScript");
+
+async function gitSongs() {
+    let a = await fetch("http://127.0.0.1:3000/songs/");
+    let response = await a.text();
+    console.log(response);
+
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let songs = [];
+
+    for (let index = 0; index < as.length; index++) {
+        const element = as[index];
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href);
+        }
+    }
+
+    return songs
+}
+
+gitSongs();
