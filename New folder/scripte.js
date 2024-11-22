afb4561e2e38a495a197cf52bce12403dea8dc3f
Fahima Azizi <fahimaazizi397@gmail.com>
2024-11-22 23:04:11 +0500

    commit

--- New folder/scripte.js
+++ New folder/scripte.js
@@ -1,6 +1,9 @@
 document.getElementById('loginForm').addEventListener('submit', function (e) {
     e.preventDefault();
-  
+    function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('active');
+  }
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
     const errorMessage = document.getElementById('errorMessage');
@@ -14,4 +17,5 @@ document.getElementById('loginForm').addEventListener('submit', function (e) {
       errorMessage.textContent = 'Invalid Gmail or password. Please try again.';
     }
   });
+ 
   
